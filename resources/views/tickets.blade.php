75ab686187b93005938b216a9af4c99c05f0acbd
gustavorieg <gustavorieg10@gmail.com>
2023-12-13 17:30:22 -0300

    1.0

--- resources/views/tickets.blade.php
+++ resources/views/tickets.blade.php
@@ -6,8 +6,24 @@
 
 @section('content')
 
-    <div class="">
-
+    <div class="tit-ticket">
+        <div class="icon-suporte">
+            <div class="icon-ticket">
+                <i class="fa-solid fa-headset"></i></li>
+            </div>
+            <div class="suporte-msg">
+                <div class="tit-suporte">
+                    <h1>Tickets</h1>
+                </div>
+                <div class="desc-suporte">
+                    <p>Bem-vindo ao Admin Dashboard</p>
+                </div>
+            </div>
+        </div>
+        <div class="box-novo-ticket">
+            <button><i class="fa-solid fa-user-plus"></i> Novo Ticket</button>
+        </div>
+        
     </div>
 
 @endsection
